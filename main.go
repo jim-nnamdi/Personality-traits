20d821584c685e5559d90debb00bf37b3dc274cc
jim-nnamdi <jimsamuel50@gmail.com>
2022-03-05 19:56:25 +0100

    + populate errorcheck and dbconn function

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
-func ErrorCheck() {
+import "database/sql"
 
+func ErrorCheck(err error) {
+	if err != nil {
+		panic(err.Error())
+	}
 }
 
-func DatabaseConnection() {
-
+func DatabaseConnection() (db *sql.DB) {
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:8889)/personalitytest")
+	ErrorCheck(err)
+	return db
 }
 
 func main() {
