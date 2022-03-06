79a6c39faca76e88590508f15ef7aafd8b83fa0a
jim-nnamdi <jimsamuel50@gmail.com>
2022-03-06 16:38:55 +0100

    -- clean up tests

--- testmain_test.go
+++ testmain_test.go
@@ -1,13 +1,1 @@
 package main
-
-import "testing"
-
-func TestReturnAllPersonalityQuestions(t *testing.T) {
-
-	/** use the table testing style to test*/
-	getThePersonalityTestResult := returnAllPersonalityQuestions()
-
-	for _, questions := range getThePersonalityTestResult {
-
-	}
-}
